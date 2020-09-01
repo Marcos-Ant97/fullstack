c59537d23d0c57d2cf4aab2fb613e63d1496e13f
Marcos Sousa <marcos@inovags.com>
2020-09-01 14:55:09 -0300

    Protótipo criar perguntas

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
 const express = require("express");
+const jsonServer = require("json-server");
+const fetch = require("node-fetch");
 const app = express();
 
 app.set('view engine','ejs');
 app.use(express.static('public'));
-server.use('/db', jsonServer.router('db.json'));
+app.use('/db',jsonServer.router('db/db.json'));
 
 app.get("/",(req,res) => {
 
-    var questionarios = [
-        {titulo: "Doritos"},
-        {titulo: "Coca-Cola"},
-        {titulo: "Leite"}
-    ]
+    /*let url = "localhost:8080/questionarios";
+
+    let settings = { method: "Get" };
+
+    fetch(url, settings).then(res => res.json()).then((json) => {
+        console.log(json);
+    });*/
+
+    //var questionarios = ;
 
     res.render("home",{
         questionarios: questionarios
---
+++ public/js/script.js
@@ -0,0 +1,48 @@
+inicializaBotaoNovaPergunta();
+
+function inicializaBotaoNovaPergunta(){
+    let botoes = document.querySelectorAll('.btn-nova-pergunta');
+
+    for(var i=0; i<botoes.length; i++){
+        botoes[i].addEventListener('click', novaPergunta);
+    }
+}
+
+function novaPergunta(){
+    
+    let divPerguntas = document.getElementById('blocoPerguntas');
+
+    let colWrap = document.createElement('div');
+    colWrap.className = 'col-xs-12 col-sm-12 col-md-6 mb-3';
+
+    let rowWrap = document.createElement('div');
+    rowWrap.className = 'row';
+
+    let colPergunta = document.createElement('div');
+    colPergunta.className = 'col-xs-11';
+
+    let colBotaoRemover = document.createElement('div');
+    colBotaoRemover.className = 'col-xs-1';
+
+    let inputPergunta = document.createElement('input');
+    inputPergunta.className = 'input-pergunta';
+    inputPergunta.type = 'text';
+
+    let botaoRemover = document.createElement('a');
+    botaoRemover.className = 'btn-remover-pergunta';
+    botaoRemover.textContent = 'X';
+    botaoRemover.addEventListener('click', removerPergunta);
+
+    colPergunta.append(inputPergunta);
+    colBotaoRemover.append(botaoRemover);
+    rowWrap.append(colPergunta);
+    rowWrap.append(colBotaoRemover);
+    colWrap.append(rowWrap);
+
+    divPerguntas.append(colWrap);
+
+}
+
+function removerPergunta(){
+
+}
